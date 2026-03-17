6cf7d4062a187a04a7223e9606fb312e82e0d05e
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-17 11:39:55 +0000

    feat: completed task 4

---
+++ Vehicles/Engine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    internal abstract class Engine
+    {
+        public bool Running { get; set; }
+        public int Horsepower { get; set; }
+        public FuelType FuelType { get; set; }
+        public Engine(int horsepower, FuelType fuelType)
+        {
+            this.Horsepower = horsepower;
+            this.FuelType = fuelType;
+        }
+    }
+}
--- Vehicles/Motorcycle.cs
+++ Vehicles/Motorcycle.cs
@@ -6,7 +6,7 @@ namespace Vehicles
 {
     internal class Motorcycle : Vehicle
     {
-        public bool HasSidecar = false;
+        public bool HasSidecar { get; set; }
         public Motorcycle(string make, string model, int speed, bool hasSidecar) : base(make, model, speed) 
         {
             this.HasSidecar = hasSidecar;
