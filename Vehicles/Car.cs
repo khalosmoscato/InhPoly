d0ce506794a4ca7fc63a826d22b21caa352c9ec2
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-17 11:22:47 +0000

    feat: completed task 2

---
+++ Vehicles/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    internal class Car : Vehicle
+    {
+        public Car(string make, string model, int speed) : base(make, model, speed) { } 
+    }
+}
