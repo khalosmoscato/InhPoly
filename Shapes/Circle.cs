1b6d6846acb4bf778a87bbaa4bcd4195d9292316
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-17 14:46:09 +0000

    feat: completed task 15 & 16 -> created 2 sunclasses of Shape called Circle and Rectangle

---
+++ Shapes/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    internal class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double CalculateArea()
+        {
+            return Radius;
+        }
+    }
+}
---
+++ Shapes/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    internal class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public override double CalculateArea()
+        {
+       
+            return Length * Width;
+        }
+    }
+}
