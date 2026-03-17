bf8019fba12f00a5f6bd7c079722a371209e01ac
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-17 11:26:09 +0000

    feat: completed task 3

---
+++ Vehicles/Motorcycle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    internal class Motorcycle : Vehicle
+    {
+        public bool HasSidecar = false;
+        public Motorcycle(string make, string model, int speed, bool hasSidecar) : base(make, model, speed) 
+        {
+            this.HasSidecar = hasSidecar;
+        }
+    }
+}
