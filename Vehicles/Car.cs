1cefdc9ad7ffab3febef74a5d39243f3d1796f6c
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-17 14:03:14 +0000

    feat: completed task 12 -> implemented Accelerate() into the subclasses Car and Motorcycle

--- Vehicles/Car.cs
+++ Vehicles/Car.cs
@@ -6,6 +6,22 @@ namespace Vehicles
 {
     internal class Car : Vehicle
     {
-        public Car(string make, string model, Engine engineType) : base(make, model, new CarEngine()) { } 
+        public Car(string make, string model, Engine engineType) : base(make, model, new CarEngine()) { }
+        
+        public void Accelerate()
+        {
+            string message = $"{Make} {Model} speed: ";
+            double fuelMod = EngineType.FuelType switch
+            {
+                FuelType.Unleaded => 1.5D,
+                FuelType.Leaded => 1.2D,
+                FuelType.Diesel => 1.8D,
+                _ => throw new InvalidOperationException($"Unexpected value: {EngineType.FuelType}")
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{message}{((EngineType.HorsePower * fuelMod) * Math.Exp(Math.Sqrt(i)) / 2):F2}mph");
+            }
+        }
     }
 }
--- Vehicles/Motorcycle.cs
+++ Vehicles/Motorcycle.cs
@@ -11,5 +11,21 @@ namespace Vehicles
         {
             this.HasSidecar = hasSideCar;
         }
+
+        public void Accelerate()
+        {
+            string message = $"{Make} {Model} speed: ";
+            double fuelMod = EngineType.FuelType switch
+            {
+                FuelType.Unleaded => 1.5D,
+                FuelType.Leaded => 1.2D,
+                FuelType.Diesel => 1.8D,
+                _ => throw new InvalidOperationException($"Unexpected value: {EngineType.FuelType}")
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{message}{((EngineType.HorsePower * fuelMod) * Math.Exp(Math.Sqrt(i)) / 2) * (HasSidecar ? 0.8 : 1.0)}mph");
+            }
+        }
     }
 }
