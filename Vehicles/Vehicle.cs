cd5885881c7622c41a29182240bd38a5bc077a7c
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-17 11:18:56 +0000

    Initialised project

---
+++ Vehicles/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    internal abstract class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+    }
+}
