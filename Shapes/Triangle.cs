0647c0eb427daeee15bb875f81c2ab54c0e04ca5
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-17 14:49:42 +0000

    feat: completed task 17 -> created new Triangle class

---
+++ Shapes/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    internal class Triangle : Shape
+    {
+        public double BaseLength { get; set; }
+        public double Height { get; set; }
+
+        public override double CalculateArea()
+        {
+            return (BaseLength * Height) / 2;
+        }
+    }
+}
